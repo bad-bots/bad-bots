ec34e4dbeb0268b05089262406e1d256981eadda
Duncan <duncangichimu@gmail.com>
2019-01-23 17:03:41 -0800

    fix bug

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -6,7 +6,7 @@ import { createStructuredSelector } from 'reselect';
 import Button from '@material-ui/core/Button';
 
 import withTransition from '../Wrappers/withTransition';
-import withBackground, {Wrapper} from '../Wrappers/withBackground';
+import withBackground, { Wrapper } from '../Wrappers/withBackground';
 
 import LoginForm from './Form';
 import { loginUser } from './actions';
@@ -19,7 +19,7 @@ const Login = props => {
     <div className="auth-page">
       <div className="auth-form-wrapper">
         <div className="auth-logo">
-        <img src="/images/logo.svg" alt="B4dB0tz" />
+          <img src="/images/logo.svg" alt="B4dB0tz" />
         </div>
         <h3 className="auth-title">Login</h3>
         <LoginForm
@@ -28,17 +28,17 @@ const Login = props => {
           loginError={props.loginError}
         />
 
-      <div className="social-logins">
-        <Button
-          variant="contained"
-          color="secondary"
-          label="Submit"
-          type="submit"
-          className="auth-submit-button"
-        >
-          Login with Google
-        </Button>
-      </div>
+        <div className="social-logins">
+          <Button
+            variant="contained"
+            color="secondary"
+            label="Submit"
+            type="submit"
+            className="auth-submit-button"
+          >
+            Login with Google
+          </Button>
+        </div>
         <div className="auth-alt-option">
           <span className="text">Dont have an account?</span>
           <Link to="/register">Register</Link>
