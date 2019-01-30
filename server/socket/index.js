a015e70d346c0ef7118d12eca82fee150defb48c
duncan-G <duncangichimu@gmail.com>
2019-01-30 15:01:46 -0800

    merge with master

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,44 +1,23 @@
-const gameState = require("../db/inMemoryDb");
+const gameState = require('../db/inMemoryDb');
 
 module.exports = io => {
-  io.on("connection", socket => {
-    console.log(
-      `A socket connection to npm server has been made: ${socket.id}`
-    );
+  io.on('connection', socket => {
+    console.log(`A socket connection to npm server has been made: ${socket.id}`)
 
-    socket.on("disconnect", () => {
-      console.log(`Connection ${socket.id} has left the building`);
-    });
+    socket.on('disconnect', () => {
+      console.log(`Connection ${socket.id} has left the building`)
+    })
 
-    socket.on("test", () => {
+    socket.on('test', () => {
       console.log(`Client ${socket.id} test`);
-      io.emit("test");
-    });
+      io.emit('test')
+    })
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    socket.on('init', roomName => {
-=======
     socket.on('init', (roomName, ack) => {
->>>>>>> Stashed changes
       socket.join('room:' + roomName);
-=======
-    socket.on("init", (roomName, ack) => {
-      socket.join(roomName);
->>>>>>> 3316c734bac0f9c05dcd3f3dd1620e6376293661
       const gameRoom = gameState.initGameRoom(roomName);
-      gameState.addPlayerOne(gameRoom, socket.id, [1, 1, 1]);
-      ack(gameRoom.joinToken);
+      gameState.addPlayerOne(gameRoom, socket.id, [1,1,1]);
       console.log(`Client ${socket.id} has started room "${roomName}"`);
-<<<<<<< Updated upstream
-    });
-
-    socket.on("start", joinToken => {
-      const gameRoom = gameState.getRoomByToken(joinToken.toString());
-      if (!gameRoom) {
-        socket.emit("incorrectGameToken");
-        return;
-=======
       ack(gameRoom.joinToken);
     })
 
@@ -48,16 +27,13 @@ module.exports = io => {
       if (!gameRoom){
         socket.emit('incorrectGameToken')
         return
->>>>>>> Stashed changes
       }
-      socket.join(gameRoom.roomName);
-      gameState.addPlayerTwo(gameRoom, socket.id, [1, 1, 1]);
+      gameState.addPlayerTwo(gameRoom, socket.id, [1,1,1]);
       gameState.startGame(gameRoom);
-      io.to(gameRoom.roomName).emit("start", gameRoom);
       console.log(`Client ${socket.id} has joined game. Game has started.`);
-    });
+    })
+    // e7a7n5ot4
 
-<<<<<<< HEAD
     // Old implementation
     socket.on('spawn unit', data => {
       console.log(data)
@@ -94,11 +70,3 @@ module.exports = io => {
 
   })
 }
-=======
-    socket.on("spawn", data => {
-      console.log("Player request unit spawn", data);
-      io.emit("spawn", data);
-    });
-  });
-};
->>>>>>> 3316c734bac0f9c05dcd3f3dd1620e6376293661
