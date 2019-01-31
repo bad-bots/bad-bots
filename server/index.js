dc225e6875172a8d49377c086d869b451908af26
Scott Techau <scotttechau@gmail.com>
2019-01-31 10:20:15 -0800

    feat(logging): Log local ip address if server is started in dev mode

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const app = require('./app');
-const db = require('./db');
-const sessionsStore = require('./sessionsStore');
-const socketio = require('socket.io');
+const app = require("./app");
+const db = require("./db");
+const sessionsStore = require("./sessionsStore");
+const socketio = require("socket.io");
 
 const PORT = process.env.SERVER_PORT || 8080;
 
@@ -10,20 +10,24 @@ db.authenticate()
     return sessionsStore.sync();
   })
   .then(() => {
-    console.log('Session store has been synced');
-    console.log('Connection to database established');
+    console.log("Session store has been synced");
+    console.log("Connection to database established");
     return db.sync();
   })
   .then(() => {
-    console.log('Database has been synced');
+    console.log("Database has been synced");
 
     const server = app.listen(PORT, () => {
-      console.log('Listening on port', PORT);
+      let address = PORT;
+      if (process.env.NODE_ENV === "development") {
+        address = `${require("ip").address("public", "ipv4")}:${PORT}`;
+      }
+      console.log("Listening on port", address);
     });
 
     const io = socketio(server);
-    require('./socket')(io);
+    require("./socket")(io);
   })
   .catch(err => {
-    console.error('Unable to start server', err);
+    console.error("Unable to start server", err);
   });
